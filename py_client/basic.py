16eea29bd8f5f6005747696614efc1bdd834d118
kaanrepo <gkaanatsever@gmail.com>
2023-07-08 18:13:21 +0300

    added workflow

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,10 @@
 import requests
 
-endpoint = 'https://httpbin.org/anything'
+#endpoint = 'https://httpbin.org/anything'
+endpoint = 'http://127.0.0.1:8000/'
 
 get_response = requests.get(endpoint, json= {'query':'hello q'})
 
 print(get_response.json())
+
+print(get_response.status_code)
